2862c81a4907ce82e88835aafbe54316f6c9f022
rutyak <132219103+rutyak@users.noreply.github.com>
2023-09-30 11:53:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const line = document.getElementById('line');
+let rotationAngle = 0;
+
+function rotateLine() {
+    rotationAngle += 2; 
+    line.style.transform = `translateX(-50%) rotate(${rotationAngle}deg)`;
+    requestAnimationFrame(rotateLine);
+}
+
+rotateLine();
